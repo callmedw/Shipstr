c5cdd52ec60a3523a79edfe830aad4f488889ca3
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-09-13 19:53:24 -0700

    include seed data and prepare seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib/data/rate_data.csv'))
+csv = CSV.parse(csv_text.scrub, headers: true)
+csv.each do |row|
+  s = ShippingRate.new
+  s.rate = row["rate"]
+  s.currency = row["currency"]
+  s.origin = row["origin"]
+  s.destination = row["destination"]
+  s.service_provider_id = row["shipping company id"]
+
+  s.save
+end
+
+puts "there are now #{ShippingRate.count} rows in the services table"
+
+
+csv_text = File.read(Rails.root.join('lib/data/service_provider_data.csv'))
+csv = CSV.parse(csv_text.scrub, headers: true)
+csv.each do |row|
+  s = ServiceProvider.new
+  s.id = row['id']
+  s.name = row["name"]
+  s.flat_rate = row["flat shipping rate"]
+  s.currency = row["currency"]
+
+  s.save
+end
+
+puts "there are now #{ServiceProvider.count} rows in the services table"
