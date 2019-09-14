341890f867aa5813cd3cb691c517bc87bc149d39
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-09-13 19:52:07 -0700

    basic models setup

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -1,2 +1,6 @@
 class ServiceProvider < ApplicationRecord
+  belongs_to :service_providers
+  validates_presence_of :name, :flat_rate, :currency
+
+
 end
--- app/models/shipping_rate.rb
+++ app/models/shipping_rate.rb
@@ -1,2 +1,4 @@
 class ShippingRate < ApplicationRecord
+  belongs_to :service_providers
+  validates_presence_of :rate, :currency, :origin, :destination, :service_provider_id, presence: true
 end
