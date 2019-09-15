9c3d7ac91761727a68caa4c5c3a89465a23166a0
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-09-14 20:16:31 -0700

    routes set

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :service_providers do
-    resources :shipping_rates
-  end
-
   resources :shipping_rates do
     resources :service_providers
   end
+
+  resources :service_providers
 end
